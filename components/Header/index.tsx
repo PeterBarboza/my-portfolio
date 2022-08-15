234268992fdf1098889b7cd3902ba0391d1e8291
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-08-15 19:38:14 -0300

    CollapsedMenu shadow added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,13 +1,23 @@
-import { useState } from "react"
+import { useLayoutEffect, useState } from "react"
 import { CollapsedMenu } from "../CollapsedMenu"
 import styles from "./styles.module.scss"
 
 export function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
+  const [pageHeight, setPageHeight] = useState<number>(0)
 
   function handleMenu() {
+    console.log(pageHeight, isMenuOpen)
     setIsMenuOpen(prevState => !prevState)
   }
+
+  useLayoutEffect(() => {
+    console.log("Renderizou")
+
+    if(pageHeight != document.body.scrollHeight) {
+      setPageHeight(document.body.scrollHeight)
+    }
+  }, [pageHeight])
 
   return (
     <header className={styles.header}>
@@ -20,6 +30,20 @@ export function Header() {
         <p>Contatos</p>
       </div>
       <CollapsedMenu isOpen={isMenuOpen} handleMenu={handleMenu} />
+      <div 
+        className={styles.menuShadow}
+        style={isMenuOpen? 
+          {
+            height: `${pageHeight}px`,
+            width: "100%"
+          } 
+          : 
+          {
+            height: `${pageHeight}px`,
+            width: "0%"
+          }
+        }
+      ></div>
     </header>
   )
 }
