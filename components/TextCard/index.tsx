1c85b9a918cef7395a75616c1b9ddf4a4879c78f
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-08-23 16:16:03 -0300

    attachments icons added

--- components/TextCard/index.tsx
+++ components/TextCard/index.tsx
@@ -1,25 +1,48 @@
-import { ReactNode } from "react"
-
-import styles from "./styles.module.scss"
+import { IconContext } from "react-icons";
+import { AiFillGithub } from "react-icons/ai";
+import { GrAttachment } from "react-icons/gr";
+import styles from "./styles.module.scss";
 
 type props = {
-  heading: string
-  projects: string[]
-}
+  heading: string;
+  text: string | string[];
+  githubLink?: string;
+  deployLink?: string;
+};
 
-export function TextCard({ heading, projects }: props) {
+export function TextCard({ heading, text, githubLink, deployLink }: props) {
   return (
     <div className={styles.textCard}>
       <h3>{heading}</h3>
-      <div className={styles.list}>
-        {
-          projects.map((item, index) => {
-            return (
-              <div key={index}>- {item}</div>
-            )
-          })
-        }
+      {Array.isArray(text) ? (
+        text.map((item, index) => {
+          return <p key={index}>{text}</p>;
+        })
+      ) : (
+        <p>{text}</p>
+      )}
+      <div className={styles.attachmentsBox}>
+        {githubLink ? (
+          <a href="" rel="noreferrer">
+            <div className={styles.icon}>
+              <IconContext.Provider value={{ size: "80%" }}>
+                <AiFillGithub color="#000000" />
+              </IconContext.Provider>
+            </div>
+            <p>Repositório</p>
+          </a>
+        ) : null}
+        {deployLink ? (
+          <a href="" rel="noreferrer">
+            <div className={styles.icon}>
+              <IconContext.Provider value={{ size: "70%" }}>
+                <GrAttachment color="#000000" />
+              </IconContext.Provider>
+            </div>
+            <p>Deploy</p>
+          </a>
+        ) : null}
       </div>
     </div>
-  )
+  );
 }
