7bc44409fb3cc88c60307306d22ef40a363bab43
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-09-01 04:06:49 -0300

    'see more' button added

--- components/ProjectsSection/index.tsx
+++ components/ProjectsSection/index.tsx
@@ -6,14 +6,54 @@ import styles from "./styles.module.scss"
 type props = {
   children: ReactNode
   heading: string
+  hideExceededContent: boolean
+  isContentHidden?: boolean
+  setIsHidden?: () => void
 }
 
-export function ProjectsSection({ heading, children }: props) {
+export function ProjectsSection({
+  heading,
+  children,
+  hideExceededContent,
+  setIsHidden = () => { },
+  isContentHidden = false
+}: props) {
+
+  if (hideExceededContent) {
+    return (
+      <DefaultSection
+        sectionStyles={{
+          backgroundImage: "none",
+        }}
+        wrapperStyles={
+          isContentHidden ? {
+            maxHeight: "70vh",
+            overflowY: "hidden",
+            position: "relative"
+          } : {}}
+      >
+        <h2>{heading}</h2>
+        {children}
+        {
+          isContentHidden ?
+            <div className={styles.seeMore} onClick={() => setIsHidden()}>
+              <p>Ver mais</p>
+            </div>
+            :
+            <div className={styles.seeLess} onClick={() => setIsHidden()}>
+              <p>Ver menos</p>
+            </div>
+        }
+      </DefaultSection>
+    )
+  }
+
   return (
     <DefaultSection
       sectionStyles={{
-        backgroundImage: "none"
-      }}>
+        backgroundImage: "none",
+      }}
+    >
       <h2>{heading}</h2>
       {children}
     </DefaultSection>
