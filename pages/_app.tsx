0168e222722567f1c1be48a7cb25cb6be19a5d64
PeterBarboza <pedro.barboza.dev@gmail.com>
2022-10-07 11:07:38 -0300

    feat: Google analytics implemented

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+import * as gtag from "../utils/gtag"
+
+import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const router = useRouter()
+
+	useEffect(() => {
+		const handleRouteChange = (url: URL) => {
+			gtag.pageview(url)
+		}
+
+		console.log("Passou no develop")
+		if (process.env.NODE_ENV !== "production") return
+		console.log("Passou no production")
+
+		router.events.on("routeChangeComplete", handleRouteChange)
+
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange)
+		}
+	}, [router.events])
+
+	return <Component {...pageProps} />
 }
 
 export default MyApp
